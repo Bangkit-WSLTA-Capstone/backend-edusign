bd3f54371a01c36d63c6287938d086f5a48910eb
kadesatrya <kade.satrya@ui.ac.id>
2024-06-15 05:59:01 +0700

    feat: removed hardcoded port and host server in app.js

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ require('dotenv').config()
 
 const init = async () => {
         const server = Hapi.server({
-        port: 3000,
-        host: 'localhost',
+        port: process.env.PORT,
+        host: process.env.HOST,
     });
     
     // Setup JWT
